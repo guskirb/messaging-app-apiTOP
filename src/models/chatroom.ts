0f0b864b9ad049a7bae38b29ac55491e7b55a2ff
guskirb <anguskirby1998@gmail.com>
2024-07-16 05:08:21 +0100

    Add last_active_formatted virtual to chatroom

--- src/models/chatroom.ts
+++ src/models/chatroom.ts
@@ -1,12 +1,23 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
+import { DateTime } from "luxon";
 
-const ChatRoomSchema = new Schema({
-  name: { type: String },
-  users: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
-  pinned: { type: Boolean, default: false },
-  last_message: { type: String },
-  last_active: { type: Date, default: new Date() },
+const ChatRoomSchema = new Schema(
+  {
+    name: { type: String },
+    users: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+    pinned: { type: Boolean, default: false },
+    last_message: { type: String },
+    last_active: { type: Date, default: new Date() },
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+ChatRoomSchema.virtual("last_active_formatted").get(function () {
+  return DateTime.fromJSDate(this.last_active).toRelative();
 });
 
 const ChatRoomModel = mongoose.model("ChatRoom", ChatRoomSchema);
