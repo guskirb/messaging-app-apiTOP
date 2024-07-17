a05a90c1d00268f1108e9d6672fb33ec91df0f8e
guskirb <anguskirby1998@gmail.com>
2024-07-17 05:01:09 +0100

    Add date formatted field to message model

--- src/models/message.ts
+++ src/models/message.ts
@@ -1,11 +1,25 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
+import { DateTime } from "luxon";
 
-const MessageSchema = new Schema({
-  message: { type: String, required: true },
-  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  chatroom: { type: Schema.Types.ObjectId, ref: "ChatRoom", required: true },
-  date: { type: Date, default: new Date() },
+const MessageSchema = new Schema(
+  {
+    message: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    chatroom: { type: Schema.Types.ObjectId, ref: "ChatRoom", required: true },
+    date: { type: Date, default: new Date() },
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
+
+MessageSchema.virtual("date_formatted").get(function () {
+  return {
+    date: DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_SHORT),
+    time: DateTime.fromJSDate(this.date).toLocaleString(DateTime.TIME_SIMPLE),
+  };
 });
 
 const MessageModel = mongoose.model("Message", MessageSchema);
